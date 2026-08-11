b794e671630983641071285f0dd7ac2fd2ce5c75
zoumingchu <zmc_1119@qq.com>
2026-08-11 23:59:46 +0800

    Add PWA install, fonts, cute polish

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'parler-v1';
+const ASSETS = ['./', './index.html', './app.css', './app.js', './highfreq.js', './vocab-ext.js', './vendor/lucide.min.js', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
+self.addEventListener('install', function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }).then(function () { return self.skipWaiting(); }));
+});
+self.addEventListener('activate', function (e) {
+  e.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(function (cached) {
+    return cached || fetch(e.request).then(function (res) {
+      var copy = res.clone();
+      caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+      return res;
+    }).catch(function () { return caches.match('./index.html'); });
+  }));
+});
